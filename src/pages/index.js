f184227f7851d5906e2891fa876f39509f8ce8ea
clementcogo <cogo.clement@live.fr>
2023-12-08 00:23:46 +0100

    juste prix game

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import Link from "next/link";
+// pages/index.js
 
-export default function Page() {
-    return <Link href={"/test/test"}>GO</Link>
-}
+import Link from 'next/link';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>INDEX JS</h1>
+      {/* Votre contenu de la page ici */}
+      
+      {/* Exemple de lien vers une autre page */}
+      <Link href="/games/justeprix">
+        JUSTE PRIX
+      </Link>
+
+      {/* Ajoutez l'icône de la langouste dans la barre du navigateur (favicon) */}
+      <link rel="icon" type="image/svg+xml" href="../lobster-svgrepo-com.svg" />
+    </div>
+  );
+};
+
+export default HomePage;
