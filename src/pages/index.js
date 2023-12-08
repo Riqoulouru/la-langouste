6bfc484f4036d1f17ed7cc7b2e0844ba4a31b624
Basile <riqoulouru@gmail.com>
2023-12-08 02:33:52 +0100

    fix clement le nullos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,3 @@
-
-// pages/index.js
-import React from 'react';
-import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
@@ -9,13 +5,13 @@ import Konami from 'react-konami-code';
 import styles from "@/styles/Home.module.scss"
 
 const HomePage = () => {
-  const { t, i18n } = useTranslation('translation');
+    const { t, i18n } = useTranslation('translation');
 
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-  };
-  
-      const [easterEggActivated, setEasterEggActivated] = useState(false);
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    };
+
+    const [easterEggActivated, setEasterEggActivated] = useState(false);
 
     const easterEgg = () => {
         alert('Hey, you typed the Konami Code!');
@@ -31,24 +27,22 @@ const HomePage = () => {
         return () => clearTimeout(resetTimeout);
     }, [easterEggActivated]);
 
-  return (
-    <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('hello')}</p>
+    return (
+        <div>
+            <h1>{t('welcome')}</h1>
+            <p>{t('hello')}</p>
 
-      {/* Changer la langue avec des boutons */}
-      <div className={styles.test}>
-        <button onClick={() => changeLanguage('fr')}>Français</button>
-        <button onClick={() => changeLanguage('en')}>English</button>
-        <Link href={"/games/Game"}>GO</Link>
-            {!easterEggActivated && <Konami action={easterEgg} />}
-      </div>
-
-      <div>
-        <Link href="/games/justeprix/1">
-          {t('justePrix')}
-        </Link>
-      </div>
+            {/* Changer la langue avec des boutons */}
+            <div className={styles.test}>
+                <button onClick={() => changeLanguage('fr')}>Français</button>
+                <button onClick={() => changeLanguage('en')}>English</button>
+                <br/>
+                <Link href={"/games/Game"}>GO</Link>
+                {!easterEggActivated && <Konami action={easterEgg} />}
+            </div>
+        </div>
 
     );
 }
+
+export default HomePage;
