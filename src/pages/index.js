21d0d12c7cd26b80febc40e46e0f2112f7a03090
Elioh <elliott.attenborough@gmail.com>
2023-12-08 03:22:27 +0100

    la glace ça fond quand y fé cho jpp, 8h pour de la merde

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import Link from "next/link";
+import React from 'react';
+import IcebergComponent from '../components/IcebergComponent/IcebergComponent'; // Assurez-vous que le chemin est correct
 
-export default function Page() {
-    return <Link href={"/test/test"}>GO</Link>
-}
+const App = () => {
+    return (
+        <div>
+            <h1>Visualisation de l'Iceberg</h1>
+            <IcebergComponent />
+        </div>
+    );
+};
+
+export default App;
