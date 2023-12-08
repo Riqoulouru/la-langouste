b844744c3868f9ae04bb790a582bf486898e8f46
clementcogo <cogo.clement@live.fr>
2023-12-08 02:27:06 +0100

    added translation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,14 @@
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+// pages/_app.js
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '/i18n.js'; // Assurez-vous d'ajuster le chemin en fonction de votre structure de dossier
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  );
 }
+
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
 // pages/index.js
-
+import React from 'react';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+  const { t, i18n } = useTranslation('translation');
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
   return (
     <div>
-      <h1>INDEX JS</h1>
-      {/* Votre contenu de la page ici */}
-      
-      {/* Exemple de lien vers une autre page */}
-      <Link href="/games/justeprix">
-        JUSTE PRIX
-      </Link>
+      <h1>{t('welcome')}</h1>
+      <p>{t('hello')}</p>
+
+      {/* Changer la langue avec des boutons */}
+      <div>
+        <button onClick={() => changeLanguage('fr')}>Français</button>
+        <button onClick={() => changeLanguage('en')}>English</button>
+      </div>
+
+      <div>
+        <Link href="/games/justeprix/1">
+          {t('justePrix')}
+        </Link>
+      </div>
 
       {/* Ajoutez l'icône de la langouste dans la barre du navigateur (favicon) */}
       <link rel="icon" type="image/svg+xml" href="../lobster-svgrepo-com.svg" />
